1b2dc36dd8a01de4b14a6b529685e03ea58c6908
MIB-Dell <arundhatigy@gmail.com>
2023-05-01 07:31:14 +0530

    committed

--- src/test/java/GitPackage/NewClass.java
+++ src/test/java/GitPackage/NewClass.java
@@ -4,6 +4,8 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("hiiiiii");
+		System.out.println("hiiiiii");
+
 
 	}
 
---
+++ src/test/java/GitPackage/NewClass1.java
@@ -0,0 +1,9 @@
+package GitPackage;
+
+public class NewClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("helloooo");
+	}
+
+}
